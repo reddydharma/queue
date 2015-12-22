da17c8e83080f3d692008cea167f7464995e7c3a
reddy <reddy.dharmavaram@yahoo.com>
2015-12-22 11:00:33 +0530

    functions done

--- que/src/que/client.java
+++ que/src/que/client.java
@@ -3,7 +3,9 @@ package que;
 public class client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 int size=5;
+		QueueArray q=new QueueArray(size);
+		
 
 	}
 
