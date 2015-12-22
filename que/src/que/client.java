cc933fc65640bf3af08aaabfc98039abc88381c1
reddy <reddy.dharmavaram@yahoo.com>
2015-12-22 12:24:32 +0530

    function2

--- que/src/que/client.java
+++ que/src/que/client.java
@@ -5,6 +5,20 @@ public class client {
 	public static void main(String[] args) {
 		 int size=5;
 		QueueArray q=new QueueArray(size);
+		q.insert(12);
+		q.insert(2);
+		q.insert(43);
+		q.insert(7);
+		q.insert(23);
+		q.insert(12);
+		q.display();
+		int no=q.delete();
+		int no1=q.delete();
+		int no2=q.delete();
+		int no3=q.delete();
+		int no4=q.delete();
+		q.display();
+		
 		
 
 	}
